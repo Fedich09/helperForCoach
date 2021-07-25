c1a04e5d726096845f9a3712202202f83d9f3363
mfedenko <mfedenko@office.atlassian.com>
2021-07-25 21:12:47 +0300

    First commit

---
+++ src/main/java/com/helperforcoach/helperforcoach/HelperForCoachApplication.java
@@ -0,0 +1,13 @@
+package com.helperforcoach.helperforcoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelperForCoachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelperForCoachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/helperforcoach/helperforcoach/HelperForCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helperforcoach.helperforcoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelperForCoachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
